88002d34316ca1d55aa13dda25980240eb33b252
gui150ml <hunkmorto666@gmail.com>
2025-05-17 14:43:57 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "teclado.c"
+#include "teclado.h"
+
+bool mover_cima(int mat[4][4]) {
+    int aux;
+    for (int j = 0; j < 4; j++) {
+        for (int i = 1; i < 4; i++) {  
+            if (mat[i][j] == 0) {
+                aux = mat[i - 1][j];
+                mat[i - 1][j] = mat[i][j];
+                mat[i][j] = aux;
+            }
+        }
+    }
+    return true;
+}
+
+bool mover_baixo(int mat[4][4]) {
+    int aux;
+    for (int j = 0; j < 4; j++) {
+        for (int i = 2; i >= 0; i--) {  
+            if (mat[i][j] == 0) {
+                aux = mat[i + 1][j];
+                mat[i + 1][j] = mat[i][j];
+                mat[i][j] = aux;
+            }
+        }
+    }
+    return true;
+}
+
+bool mover_esquerda(int matriz[4][4]) {
+    int aux;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j < 4; j++) {  
+            if (matriz[i][j] == 0) {
+                aux = matriz[i][j - 1];
+                matriz[i][j - 1] = matriz[i][j];
+                matriz[i][j] = aux;
+            }
+        }
+    }
+    return true;
+}
+
+bool mover_direita(int matriz[4][4]) {
+    int aux;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 2; j >= 0; j--) { 
+            if (matriz[i][j] == 0) {
+                aux = matriz[i][j + 1];
+                matriz[i][j + 1] = matriz[i][j];
+                matriz[i][j] = aux;
+            }
+        }
+    }
+    return true;
+}
+
+void imprimir_matriz( int matriz[4][4]){
+ 
+ for(int i = 0; i < 4; i++ ){
+    for(int j = 0; j < 4; j++){
+        printf(" %d ", matriz[i][j]);
+    }
+    printf("\n"); 
+ }    
+}
+bool ganhou(int matriz[4][4]){
+   
+     int matri_certo[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+     int certos = 0;
+     
+    for(int i = 0; i < 4; i++ ){
+      for(int j = 0; j < 4; j++){
+      if(matri_certo[i][j] == matriz[i][j]){
+        certos = certos+1;
+   
+    }
+}
+}
+    return certos == 15;
+}
+int main(){
+  int fim = true;
+  char resposta;
+  
+  int matriz[4][4] = {
+        {1, 2, 3, 4},
+        {5, 0, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 6, 15}
+    };
+ void tec_inicia();
+ tec = tec_tecla();
+ imprimir_matriz(matriz);
+while(!ganhou(matriz)){
+printf("digite algo w s a d\n");
+
+ 
+     switch(resposta){
+         case T_CIMA:
+            mover_cima(matriz);        
+            imprimir_matriz(matriz);
+            printf("\n");
+         break;
+         
+         case T_BAIXO:
+         mover_baixo(matriz);
+         imprimir_matriz(matriz);
+         printf("\n");
+         break;
+         
+         case T_ESQUERDA:
+         mover_esquerda(matriz);
+         imprimir_matriz(matriz);
+         printf("\n");
+         break; 
+         
+         case T_DIREITA:
+         mover_direita(matriz);
+         imprimir_matriz(matriz);
+         printf("\n");
+         break;
+     }
+    
+ }
+ void tec_fim();
+ }
